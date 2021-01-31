385f17108bf5a222423bde7330c339f1946539c2
SofieTorch <sofie.torch@outlook.com>
2021-01-31 14:08:48 -0400

    Basic game over

--- Assets/sofi/scripts/PoisonsController.cs
+++ Assets/sofi/scripts/PoisonsController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PoisonsController : MonoBehaviour
 {
@@ -13,13 +14,25 @@ public class PoisonsController : MonoBehaviour
     void Start() 
     {
         poisonCount = 0;
+        poisonTxt.text = "x0";
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.transform.CompareTag("Poison")) {
-            poisonCount++;
-            poisonTxt.text = $"x{poisonCount}";
-            Destroy(other.gameObject);
+        switch(other.gameObject.tag) {
+            case "Poison":
+                Destroy(other.gameObject);
+                poisonCount++;
+                poisonTxt.text = $"x{poisonCount}";
+                break;
+
+            case "Mob":
+                Destroy(gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
+
+            default:
+                break;
         }
     }
+
 }
